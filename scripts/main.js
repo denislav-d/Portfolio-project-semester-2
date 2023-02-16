e887ff2c20347d6a7d95fc60af1f46df22e0568c
denislav-d <ddenislav333@gmail.com>
2023-02-16 18:28:54 +0100

    Fixes and hover animations

--- scripts/main.js
+++ scripts/main.js
@@ -29,6 +29,7 @@ const handleHover = function (e) {
     });
   }
 };
+
 // Passing "argument" into handler
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
