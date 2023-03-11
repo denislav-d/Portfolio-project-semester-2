1b3e1e5d962d344a7e50fae10cf99ccbc149adfd
denislav-d <ddenislav333@gmail.com>
2023-03-11 18:51:21 +0100

    Optimisation

--- scripts/main.js
+++ scripts/main.js
@@ -89,31 +89,3 @@ document.querySelectorAll(".animation").forEach(function (i) {
     observer.observe(i);
   }
 });
-
-// Intro animation
-let intro = document.querySelector(".intro");
-let logo = document.querySelector(".intro-header");
-let logoSpan = document.querySelectorAll(".intro-span");
-
-window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    logoSpan.forEach((span, index) => {
-      setTimeout(() => {
-        span.classList.add("active");
-      }, (index + 1) * 400);
-    });
-
-    setTimeout(() => {
-      logoSpan.forEach((span, index) => {
-        setTimeout(() => {
-          span.classList.remove("active");
-          span.classList.add("fade");
-        }, (index + 1) * 50);
-      });
-    }, 2000);
-
-    setTimeout(() => {
-      intro.style.top = "-100vh";
-    }, 2300);
-  });
-});
